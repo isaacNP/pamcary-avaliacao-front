9f7bf79e1693ec04100a6b1571f97d5ef320b8c7
Isaac <isaac.npcad@gmail.com>
2022-08-01 09:13:40 -0300

    Correção layout listagem

--- src/app/shared/app-material.module.ts
+++ src/app/shared/app-material.module.ts
@@ -1,6 +1,9 @@
+import { PersonsComponent } from './../persons/persons/persons.component';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -8,7 +11,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
+import { MAT_DATE_LOCALE  } from '@angular/material/core';
 
 @NgModule({
   exports: [
@@ -20,7 +23,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatButtonModule,
     MatIconModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'pt-BR'}
   ]
 })
 export class AppMaterialModule { }
